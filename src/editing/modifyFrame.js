54539b84d84418ba0007cfdc629086bb482a9b13
zila <rimsila.itc@gmail.com>
2023-07-23 16:08:10 +0700

    add flip to video

--- src/editing/modifyFrame.js
+++ src/editing/modifyFrame.js
@@ -1,14 +1,23 @@
-const modifyFram = async (frame, f, resolution) => {
+const Jimp = require("jimp");
 
+const modifyFrame = async (frame, filterImage, resolution, flipHorizontal) => {
+    // Resize the frame to the specified resolution
     const getResolution = {
+        '144': [144, 360],   // Standard 480p resolution
         '480': [480, 720],   // Standard 480p resolution
         '720': [720, 1280],  // Standard 720p resolution
         '1080': [1080, 1920] // Standard 1080p resolution
     }[resolution?.toString() ?? '1080'];
 
-    frame.resize(...getResolution);
-    frame.composite(f, 0, 0);
-    return frame;
-}
+    // Resize filter image
+    filterImage = filterImage.resize(...getResolution, Jimp.AUTO);
 
-module.exports = modifyFram
+    // Apply filter to frame
+    frame.flip(true, false);
+    frame.composite(filterImage, 0, 0);
+    frame.resize(...getResolution);
+
+    return frame;
+};
+
+module.exports = modifyFrame;
